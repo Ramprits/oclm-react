7bcf1f6ac6001f62f753121376693ef3e678a228
Ramprit Sahani <ramprit.sahan@mobileprogramming.com>
2021-04-13 20:01:31 +0530

    Adding Leads

---
+++ src/models/index.d.ts
@@ -0,0 +1,16 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+export declare class User {
+  readonly id: string;
+  readonly firstName: string;
+  readonly middleName?: string;
+  readonly lastName: string;
+  readonly createdOn: string;
+  readonly updatedOn: string;
+  constructor(init: ModelInit<User>);
+  static copyOf(source: User, mutator: (draft: MutableModel<User>) => MutableModel<User> | void): User;
+}
