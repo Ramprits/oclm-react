d707981e05afafc2506dc192ec9950eb1963e402
Ramprit Sahani <ramprit.sahan@mobileprogramming.com>
2021-04-15 21:19:11 +0530

    adding graphql

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,85 +1,34 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateUser = /* GraphQL */ `
-  subscription OnCreateUser {
-    onCreateUser {
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
       id
-      firstName
-      middleName
-      lastName
-      createdOn
-      updatedOn
+      name
+      description
       createdAt
       updatedAt
     }
   }
 `;
-export const onUpdateUser = /* GraphQL */ `
-  subscription OnUpdateUser {
-    onUpdateUser {
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
       id
-      firstName
-      middleName
-      lastName
-      createdOn
-      updatedOn
+      name
+      description
       createdAt
       updatedAt
     }
   }
 `;
-export const onDeleteUser = /* GraphQL */ `
-  subscription OnDeleteUser {
-    onDeleteUser {
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
       id
-      firstName
-      middleName
-      lastName
-      createdOn
-      updatedOn
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onCreateLead = /* GraphQL */ `
-  subscription OnCreateLead {
-    onCreateLead {
-      id
-      firstName
-      middleName
-      lastName
-      gender
-      profilePicture
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onUpdateLead = /* GraphQL */ `
-  subscription OnUpdateLead {
-    onUpdateLead {
-      id
-      firstName
-      middleName
-      lastName
-      gender
-      profilePicture
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onDeleteLead = /* GraphQL */ `
-  subscription OnDeleteLead {
-    onDeleteLead {
-      id
-      firstName
-      middleName
-      lastName
-      gender
-      profilePicture
+      name
+      description
       createdAt
       updatedAt
     }
